61a71ab727bb53fcfc9031d65dadb3e48548fccf
jonghui-js <jonghui.js@gmail.com>
2020-04-20 15:50:35 +0900

    update

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-const electron = require('electron');
-//overall app process
-const { app, BrowserWindow, Menu } = electron;
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+let keyword1 = '배달';
+let keyword2 = '클럭';
+const picker = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const adList = [];
+  await page.goto('https://m.naver.com/', { waitUntil: 'networkidle2' });
+  await page.goto(
+    'https://m.naver.com/naverapp/?cmd=onMenu&version=3&menuCode=ENT',
+    { waitUntil: 'networkidle2' }
+  );
+  let content = await page.evaluate(() => document.body.innerHTML);
+  const $ = cheerio.load(content, { decodeEntities: true });
+  const $bannerList_ent = $('div.nmap_wrapper img.banner_img');
 
-let mainWindow;
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: true
+  $bannerList_ent.each(function (i, elem) {
+    adList[i] = $(this).attr('alt');
+  });
+
+  for (let i = 0; i < adList.length; i++) {
+    console.log('실행');
+    if (keyword1 && adList[i].includes(`${keyword1}`)) {
+      await page.screenshot({
+        path: `./screenshot/${keyword1}.png`,
+        fullPage: true,
+      });
+      console.log(`${keyword1}.png가 저장되었습니다.`);
+      keyword1 = null;
+      console.log(`keyword1의 현재 값은 ${keyword1}입니다.`);
+    } else if (keyword2 && adList[i].includes(`${keyword2}`)) {
+      await page.screenshot({
+        path: `./screenshot/${keyword2}.png`,
+        fullPage: true,
+      });
+      console.log(`${keyword2}.png가 저장되었습니다.`);
+      keyword2 = null;
+      console.log(`keyword2의 현재 값은 ${keyword2}입니다.`);
     }
-  });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.on('closed', () => app.quit());
-
-  const mainMenu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(mainMenu);
-});
-
-const menuTemplate = [
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'New todo',
-        click() {
-          createAddWindow();
-        }
-      },
-      {
-        label: 'Quit',
-        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click() {
-          app.quit();
-        }
-      }
-    ]
   }
-];
+  console.log(adList);
+  await browser.close();
+};
 
-if (process.platform === 'darwin') {
-  menuTemplate.unshift({ label: '' });
-}
+/*
+const dodo = () => {
+  for (let j = 0; j < 3; j++) {
+    jerryPicker();
+  }
+  return;
+};
+dodo();
+*/
 
-if (process.env.NODE_ENV !== 'production') {
-  menuTemplate.push({
-    label: 'View',
-    submenu: [
-      {
-        label: 'Toggle Developer Tools',
-        accelerator:
-          process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
-        click(item, focusedWindow) {
-          focusedWindow.toggleDevTools();
-        }
-      }
-    ]
-  });
-}
+const jerry = (picker, interval = 7000) => {
+  setTimeout(() => {
+    if (!keyword1 && !keyword2) return;
+    picker();
+    jerry(picker, interval);
+  }, interval);
+};
+
+jerry(picker, 7000);
